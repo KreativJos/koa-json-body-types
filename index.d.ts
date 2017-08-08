2c07a30543070c1f9a4f568c605c42796a3fb743
Jos Wagenmakers <jos@kreativsoftware.com>
2017-08-08 11:51:21 +0200

    [Add] Initial index.d.ts & package.json

---
+++ index.d.ts
@@ -0,0 +1,25 @@
+// Project: https://github.com/venables/koa-json-body
+// Definitions by: Jos Wagenmakers <https://github.com/geoffreak>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node" />
+import * as CoBody from 'co-body';
+import * as Koa from 'koa';
+
+declare namespace KoaJsonBody {
+    export interface Options extends CoBody.Options {
+        /**
+         * When set to true, ctx.request.body will always contain {} upon missing or invalid payloads.
+         * 
+         * @type {boolean}
+         * @default false
+         * @memberof Options
+         */
+        fallback?: boolean;
+    }
+
+    export interface ParseMiddleware {
+        (opts?: Options): Koa.Middleware
+    }
+}
+
